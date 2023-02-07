5887e6b57f0d891da97e0249af81f1ff1994a5ca
Nina Gomez <kf.gmez@gmail.com>
2023-02-07 20:47:04 +1000

    this is my attempt at step 1

---
+++ s1.js
@@ -0,0 +1,15 @@
+// Function for adding a greeting to a message.
+function addGreeting(message) {
+ 
+    let greeting = "Hello! The message is: ";
+    // Add "Hello! The message is: " to the string.
+    return greeting + message;
+}
+ 
+unitCode = ("I am informing you of of this unit's code");
+// Send a message informing someone of this unit's code.
+newMessage = addGreeting(unitCode);
+ 
+
+// Show the message in the console.
+console.log(newMessage);
